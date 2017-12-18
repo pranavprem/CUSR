32c94a497993787ac03bf6342268bec5dbd102aa
pranavprem <pranav.prem@sjsu.edu>
2017-12-17 22:05:47 -0800

    added facebook login

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 import SearchLayout from "./js/pages/SearchLayout.js"
 import TicketLayout from "./js/pages/TicketLayout.js"
 import HistoryLayout from "./js/pages/HistoryLayout.js"
+import UserLayout from "./js/pages/UserLayout.js"
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 const app = document.getElementById('app');
@@ -13,7 +14,7 @@ ReactDOM.render(
 
     <Router>
     <div>
-      <Route exact path ="/" component={SearchLayout}/>
+      <Route exact path ="/" component={UserLayout}/>
       <Route exact path ="/ticket" component={TicketLayout}/>
       <Route exact path="/search" component={SearchLayout}></Route>
       <Route exact path="/tickethistory" component={HistoryLayout}></Route>
--- src/js/pages/UserLayout.js
+++ src/js/pages/UserLayout.js
@@ -1,33 +1,18 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FacebookLogin from 'react-facebook-login';
 
 import Header from "../components/Header.js"
 import Footer from "../components/Footer.js"
 
+const responseFacebook = (response) => {
+    console.log(response);
+  }
+
 export default class UserLayout extends React.Component {
   constructor(){
-    super();
-    this.state = {"userid":null, "ticketid":null, "ticket":null, "isloading":true};
-    //const client = require('./Restclient');
-   
+    super();  
   }
-
-  Login() {
-    console.log("it did this");
-    var url = "https://52.90.170.105:8080/"+this.state.userid+"/ticket/"+this.state.ticketid;
-    fetch(url, 
-          {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-          })
-    .then(result => result.json())
-    .then(item => this.setState({"isloading":"false", "ticket":item}));
-    console.log(this.state.ticket);
-    
-  }
-
-  
   
   render() {
     // setTimeout(()=>{
@@ -36,6 +21,13 @@ export default class UserLayout extends React.Component {
     return (
       <div className="container">
         <Header/>
+
+         <FacebookLogin
+            appId="164569124158280"
+            autoLoad={true}
+            fields="name,email"
+            onClick={componentClicked}
+            callback={responseFacebook} />
         
         <Footer/>
       </div>    
